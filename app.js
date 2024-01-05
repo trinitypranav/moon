2334591b5e65ac7eb797d3968df466ec671fb53a
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-06 00:10:34 +0530

    fs module syncor blocking way

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const log = require("./logger");
 const path = require("path");
 const os = require("os");
+const fs = require("fs");
 
 // line 1 is a module wrapper function which is an IIFE with params 'exports','__dirname','__filename','require','module'
 log("my name is Pranav");
@@ -13,3 +14,7 @@ console.log(pathObj); //root,dir,base,ext,name
 const totalMemory = os.totalmem();
 const freeMemory = os.freemem();
 console.log(`Total Memory: ${totalMemory}, Free Memory: ${freeMemory}`);
+
+//fs module
+const files = fs.readdirSync("./");
+console.log("Sync way - Current folder contains ", files); //[ '.git', 'app.js', 'logger.js' ]

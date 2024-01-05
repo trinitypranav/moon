4f5033b9e0b4aa7199c5a8cbd4d457075ce87578
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-06 00:00:48 +0530

    studying os module and template strings

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const log = require("./logger");
 const path = require("path");
+const os = require("os");
 
 // line 1 is a module wrapper function which is an IIFE with params 'exports','__dirname','__filename','require','module'
 log("my name is Pranav");
@@ -7,3 +8,8 @@ log("my name is Pranav");
 //path module
 const pathObj = path.parse(__filename);
 console.log(pathObj); //root,dir,base,ext,name
+
+//os module
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+console.log(`Total Memory: ${totalMemory}, Free Memory: ${freeMemory}`);

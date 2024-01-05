3b36eecc007341b820c0dec4f537bb81c5f52077
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-05 23:48:04 +0530

    path module and IIFE study

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const log = require("./logger");
+const path = require("path");
 
+// line 1 is a module wrapper function which is an IIFE with params 'exports','__dirname','__filename','require','module'
 log("my name is Pranav");
+
+//path module
+const pathObj = path.parse(__filename);
+console.log(pathObj); //root,dir,base,ext,name

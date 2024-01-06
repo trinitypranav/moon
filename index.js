5c132f1229749b36c90a50a2bcd912a1384dc943
Pranav Jadhav <trinitypranav@gmail.com>
2024-01-07 00:31:49 +0530

    reduce underscore

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const _ = require("underscore");
+let array = [1, 2, 3, 4, 5];
 
-const found = _.contains([1, 2, 3, 4, 5], 4);
+const found = _.contains(array, 4);
 console.log(found);
+
+var sum = _.reduce(
+  array,
+  function (memo, num) {
+    return memo + num;
+  },
+  0
+);
+console.log(sum);
